1cb81c2f896e4929d7335ccd94d6ce8f2248667d
EKZO729 <falecom.marcopolo@gmail.com>
2020-08-24 02:50:07 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* TODO revisar calculo de amotizacao */
+/* FIXME  revisar calculo de amotizacao */
+
+/**@author Marcopolo <marcopolo@provedor.com.br> */
+
+phpinfo();
+phpinfo();
+phpinfo();
+phpinfo();
+phpinfo();
+phpinfo();
+phpinfo();
+
+
+
+
+
+
+
